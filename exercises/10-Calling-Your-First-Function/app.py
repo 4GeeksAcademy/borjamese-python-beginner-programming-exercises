34345c0c1ddc6c7e749d8dd9fa466e121cb45fbd
Borjamese <119245691+Borjamese@users.noreply.github.com>
2023-05-06 17:44:52 +0000

    ejercicios

--- exercises/10-Calling-Your-First-Function/app.py
+++ exercises/10-Calling-Your-First-Function/app.py
@@ -1,6 +1,9 @@
-def is_odd(my_number):
-  	return (my_number % 2 != 0)
-
-
 def my_main_code():
-    # your code here
+    def is_odd(my_number):
+        return (my_number % 2 != 0)
+  
+    result = is_odd(45345)
+    print("The number is odd:", result)
+    return result
+
+my_main_code()
--- exercises/11-Create-A-New-Function/app.py
+++ exercises/11-Create-A-New-Function/app.py
@@ -1,3 +1,6 @@
 import random
 
 # your code here
+def generate_random():
+    rn = random.randint(0,9)
+    return(rn)
